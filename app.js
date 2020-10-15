95597d32d5bfdaf8b4d892c934e0ccc18c083372
dahas <dahas@gmx.de>
2020-10-15 17:20:09 +0200

    .

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.set('view engine', 'ejs');
 
