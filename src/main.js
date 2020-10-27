b55bd6e7a4e2182b62bf7ec54000e2daf689d654
dahas <dahas@gmx.de>
2020-10-27 17:28:54 +0100

    .

--- src/main.js
+++ src/main.js
@@ -5,9 +5,21 @@ require('bootstrap');
 
 $(() => {
     // ---- Add your stuff below ----
+    
     console.log("Document is ready!");
 
-    $(function () {
-        $('[data-toggle="popover"]').popover()
-    })
+    // Enable popovers:
+    $('[data-toggle="popover"]').popover();
+
+    /* Inline event handler must be made global 
+       by attaching them to the windows object! */
+    window.showAlert = () => {
+        alert("OK!")
+    }
+
+    /* To avoid the above behaviour you better 
+       attach events like shown below: */
+    // $('#alert').on('mouseup', () => {
+    //     alert("OK!");
+    // });
 });
