3b3c1edd4eb3c2685edfe11ef40ca0a4642c07a1
dahas <dahas@gmx.de>
2020-11-10 21:07:11 +0100

    .

--- sw-template.js
+++ sw-template.js
@@ -4,12 +4,6 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// Add resources that aren´t cached automatically here:
-// const routes = [
-//     { "url": "/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2" },
-//     { "url": "/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2" }
-// ];
-
 workbox.setConfig({ debug: true });
 
 workbox.core.skipWaiting();
