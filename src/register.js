cc845b5a8117f4cc71638889c45d9dbd4293e9f2
dahas <dahas@gmx.de>
2020-11-10 10:34:24 +0100

    .

--- src/register.js
+++ src/register.js
@@ -3,11 +3,21 @@ window.$ = window.jQuery = require('jquery');
 import 'popper.js';
 import 'bootstrap';
 
+import { Workbox } from 'workbox-window';
+
 const enableServiceWorker = true;
 
 if (enableServiceWorker && 'serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js')
-            .then(() => console.log("ServiceWorker registered!"))
+    const wb = new Workbox('sw.js');
+
+    wb.addEventListener('installed', event => {
+        console.log("# ServiceWorker installed:", event)
+        if (event.isUpdate) {
+            if (confirm(`New content is available!. Click OK to refresh`)) {
+                window.location.reload();
+            }
+        }
     });
+
+    wb.register().then(() => console.log("# ServiceWorker registered!"));
 }
--- sw-template.js
+++ sw-template.js
@@ -6,10 +6,16 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 // Add resources that aren´t cached automatically here:
 const routes = [
-    {"url": "/"}, 
-    {"url": "/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2"},
-    {"url": "/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2"}
+    { "url": "/" },
+    { "url": "/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2" },
+    { "url": "/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2" }
 ];
+
+workbox.setConfig({ debug: true })
+
+// Updating SW lifecycle to update the app after user triggered refresh
+workbox.core.skipWaiting()
+workbox.core.clientsClaim()
 
 workbox.precaching.precacheAndRoute(routes.concat(self.__WB_MANIFEST), {
     directoryIndex: null,
